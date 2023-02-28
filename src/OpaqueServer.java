246c347057e0029cca68396edec27496fb430b4f
Y1Daa <1147042405@qq.com>
2023-02-28 22:49:26 +0800

    大体框架搭建

--- src/OpaqueServer.java
+++ src/OpaqueServer.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class OpaqueServer {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class OpaqueServer {
+    private static final int serverPort = 9000; // 服务器端口号
+    private static final int timeout = 30;
+
+    public static void main(String[] args) throws Exception {
+        // 创建ServerSocket对象，并监听端口号
+        ServerSocket serverSocket = new ServerSocket(serverPort);
+        serverSocket.setSoTimeout(timeout);
+
+        while (true) {
+            // 接收客户端的连接请求，并创建Socket对象
+            Socket socket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // 接收客户端发送的用户名
+            String username = in.readLine();
+
+            // 生成盐值，并发送给客户端
+            // TODO: OPAQUE协议的实现
+            // String salt = OpaqueProtocol.generateSalt();
+            String salt = "salt!!!";
+            out.println(salt);
+
+            // 接收客户端发送的凭证，验证身份
+            String credential = in.readLine();
+
+            // TODO: OPAQUE协议的实现
+            // OpaqueProtocol.authenticate(username, credential)
+
+            // 关闭连接
+            socket.close();
+        }
+    }
 }
--- src/OpaqueUser.java
+++ src/OpaqueUser.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class OpaqueUser {
+public class OpaqueUser {
+    private String name;
+    private String password;
+
+    /**
+     * 测试使用的构造方法
+     */
+    public OpaqueUser() {
+        this.name = "liu";
+        this.password = "yaowen";
+    }
+
+    /**
+     * OpaqueUser类构造方法
+     *
+     * @param name     用户名
+     * @param password 密码
+     */
+    public OpaqueUser(String name, String password) {
+        // 这里的逻辑不清楚有没有问题，是要使用这个password密码进行用户注册？？
+        this.name = name;
+        this.password = password;
+    }
+
+    /**
+     * 通过FIDO用户的相关信息获取OPAQUE用户的相关信息
+     *
+     * @param FidoUser FIDO用户对应的FidoUser对象
+     * @return 返回OPAQUE用户对应的OpaqueUser对象
+     */
+
+    public OpaqueUser getUserInfo(FidoUser FidoUser) {
+        // TODO: 在数据库中通过FidoUser的用户名查询OPAQUE用户的用户名，进而获取相关信息
+        return new OpaqueUser();
+    }
 }
