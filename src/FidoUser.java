274bb6cb42eb4eeb8b16d1c43e6235acfc29375a
P0tato-82 <2312225254@qq.com>
2023-03-01 18:52:56 +0800

    init

--- src/FidoUser.java
+++
@@ -1,9 +0,0 @@
-public class FidoUser {
-    /**
-     * FIDO认证的用户类，保存用户的认证信息，包括用户名name与密码password
-     */
-    private String name;
-    private String password;
-
-    // TODO: 这里的逻辑应该有点问题，需要再改改
-}
